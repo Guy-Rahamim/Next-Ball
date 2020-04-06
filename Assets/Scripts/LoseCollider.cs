c0e32e741580491ca1eb66c4bac5f81db88ad15e
Guy rahamim <guy.danino5@gmail.com>
2020-04-06 16:15:19 +0300

    A commit before fixing main menu bug

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class LoseCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Debug.Log("lose collider");
+		SceneManager.LoadScene("Game Over");
+	}
 }
